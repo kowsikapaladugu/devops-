a394b71f0cf4eda516aea001021a36bfbd921151
kowsikapaldugu <kowsikapaladugu@gmail.com>
2024-10-16 11:58:20 +0530

    fourth commit

---
+++ sum of  numbers.py
@@ -0,0 +1,8 @@
+# Function to sum a list of numbers
+def sum_of_numbers(numbers):
+    return sum(numbers)
+
+# Example usage
+numbers = [1, 2, 3, 4, 5]
+total = sum_of_numbers(numbers)
+print("The sum is:", total)
